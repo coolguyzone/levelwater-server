eae4aeb0b40c128bb9e85878155004ca70748d22
Alex Krawiec <alexanderkrawiec@gmail.com>
2017-05-01 18:30:12 -0700

    removing auth from water systems get route

--- routes/waterSys_functions/getWaterSystem.js
+++ routes/waterSys_functions/getWaterSystem.js
@@ -5,11 +5,11 @@ const getWaterSystem = (req, res) => {
   .where('id', req.claim.userId)
   .select('water_systems_id')
   .then((result) => {
-    if (Number(req.params.water_systems_id) === Number(result[0].water_systems_id)) {
+    // if (Number(req.params.water_systems_id) === Number(result[0].water_systems_id)) {
       return knex('water_systems')
       .where('id', req.params.water_systems_id);
-    }
-    return res.send({ status: 400, ErrorMessage: 'water system not found!' });
+    // }
+    // return res.send({ status: 400, ErrorMessage: 'water system not found!' });
   })
   .then((result) => {
     res.json(result[0]);
